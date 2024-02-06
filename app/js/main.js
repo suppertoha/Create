2e11c577b95b20f91c4b0e43a4d35604079b7b9d
unknown <suppertoha@yandex.ru>
2024-02-06 18:21:46 +0300

    сделал блок reviws partners, поправил отображение слайдера портфолио

--- app/js/main.js
+++ app/js/main.js
@@ -158,31 +158,64 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var swiper__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! swiper */ "./node_modules/swiper/swiper.esm.js");
 
 swiper__WEBPACK_IMPORTED_MODULE_0__["default"].use([swiper__WEBPACK_IMPORTED_MODULE_0__.Navigation, swiper__WEBPACK_IMPORTED_MODULE_0__.Pagination]);
-//const swiper = new Swiper(el, {
-//  slidesPerView: "auto",
-//});
-
 function initSwipers() {
   var allSwipers = document.querySelectorAll('.swiper');
   allSwipers.forEach(function (swiperContainer) {
+    const portSlider = document.querySelector('.slider-cards');
     var swiper = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](swiperContainer, {
-      // Здесь вы можете настроить параметры вашего слайдера
       slidesPerView: 3,
       spaceBetween: 30,
+      allowTouchMove: false,
       navigation: {
         nextEl: '.portfolio-section__next',
         prevEl: '.portfolio-section__prev'
       },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
+      on: {
+        init: function () {
+          const activeSlide = portSlider.querySelector('.swiper-slide-active');
+          const nextActiveSlide = activeSlide.nextElementSibling;
+          const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+          activeSlide.classList.add('slider-visible');
+          nextActiveSlide.classList.add('slider-visible');
+          nextNextActiveSlide.classList.add('slider-visible');
+        }
       }
+    });
+    document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+      const activeSlide = portSlider.querySelector('.swiper-slide-next');
+      document.querySelectorAll('.swiper-slide').forEach(el => {
+        el.classList.remove('slider-visible');
+      });
+      if (activeSlide.previousElementSibling) {
+        const nextActiveSlide = activeSlide.previousElementSibling;
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        activeSlide.nextElementSibling.classList.add('slider-visible');
+      }
+    });
+    document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+      const activeSlide = portSlider.querySelector('.swiper-slide-active');
+      const nextActiveSlide = activeSlide.nextElementSibling;
+      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+      document.querySelectorAll('.swiper-slide').forEach(el => {
+        el.classList.remove('slider-visible');
+      });
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      nextNextActiveSlide.classList.add('slider-visible');
     });
   });
 }
-
-// Вызов функции для инициализации всех слайдеров
 initSwipers();
+const portSlider = document.querySelector('.slider-reviews');
+const swiperReviews = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](portSlider, {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.slider-reviews__next',
+    prevEl: '.slider-reviews__prev'
+  }
+});
 
 /***/ }),
 
--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,29 +1,74 @@
 import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
-//const swiper = new Swiper(el, {
-//  slidesPerView: "auto",
-//});
+
 
 function initSwipers() {
   var allSwipers = document.querySelectorAll('.swiper');
 
-  allSwipers.forEach(function(swiperContainer) {
+  allSwipers.forEach(function (swiperContainer) {
+    const portSlider = document.querySelector('.slider-cards');
     var swiper = new Swiper(swiperContainer, {
-      // Здесь вы можете настроить параметры вашего слайдера
       slidesPerView: 3,
       spaceBetween: 30,
+      allowTouchMove: false,
       navigation: {
         nextEl: '.portfolio-section__next',
         prevEl: '.portfolio-section__prev',
       },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
+      on: {
+        init: function () {
+          const activeSlide = portSlider.querySelector('.swiper-slide-active');
+          const nextActiveSlide = activeSlide.nextElementSibling;
+          const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+          activeSlide.classList.add('slider-visible');
+          nextActiveSlide.classList.add('slider-visible');
+          nextNextActiveSlide.classList.add('slider-visible');
+        },
       },
+
+    });
+
+    document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+      const activeSlide = portSlider.querySelector('.swiper-slide-next');
+
+      document.querySelectorAll('.swiper-slide').forEach(el => {
+        el.classList.remove('slider-visible');
+      });
+
+      if (activeSlide.previousElementSibling) {
+        const nextActiveSlide = activeSlide.previousElementSibling;
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        activeSlide.nextElementSibling.classList.add('slider-visible');
+      }
+    });
+
+    document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+      const activeSlide = portSlider.querySelector('.swiper-slide-active');
+      const nextActiveSlide = activeSlide.nextElementSibling;
+      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+      document.querySelectorAll('.swiper-slide').forEach(el => {
+        el.classList.remove('slider-visible');
+      });
+
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      nextNextActiveSlide.classList.add('slider-visible');
     });
   });
 }
 
-// Вызов функции для инициализации всех слайдеров
 initSwipers();
+
+const portSlider = document.querySelector('.slider-reviews');
+
+const swiperReviews = new Swiper(portSlider, {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.slider-reviews__next',
+    prevEl: '.slider-reviews__prev',
+  },
+});
+
 
